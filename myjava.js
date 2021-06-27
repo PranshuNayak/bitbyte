c12c12988304ceaeb309f54e34a22ed0c660f8ce
PranshuNayak <86283785+PranshuNayak@users.noreply.github.com>
2021-06-27 11:13:47 +0530

    Add files via upload
    This is the layout for website for hackathon

---
+++ myjava.js
@@ -0,0 +1,82 @@
+function myfunction1(){
+    var x=document.getElementById("a1");
+    document.getElementById("a2").style.display="none";
+    document.getElementById("a3").style.display="none";
+    document.getElementById("a4").style.display="none";
+    document.getElementById("a5").style.display="none";
+        x.style.display="flex";
+        x.style.padding="4px";
+        x.style.flexDirection="column";
+        x.style.justifyContent="space-evenly";
+        x.style.marginLeft="20%";
+        x.style.width="60%";
+        x.style.border="2px solid whitesmoke";
+        x.style.borderRadius="20px";
+        x.style.color="white";
+    
+        
+}
+function myfunction2(){
+    var x=document.getElementById("a2");
+    document.getElementById("a1").style.display="none";
+    document.getElementById("a3").style.display="none";
+    document.getElementById("a4").style.display="none";
+    document.getElementById("a5").style.display="none";
+    x.style.display="flex";
+    x.style.padding="4px";
+    x.style.flexDirection="column";
+    x.style.justifyContent="space-evenly";
+    x.style.marginLeft="20%";
+    x.style.width="60%";
+    x.style.border="2px solid whitesmoke";
+    x.style.borderRadius="20px";
+    x.style.color="white";
+        
+}
+function myfunction3(){
+    var x=document.getElementById("a3");
+    document.getElementById("a1").style.display="none";
+    document.getElementById("a2").style.display="none";
+    document.getElementById("a4").style.display="none";
+    document.getElementById("a5").style.display="none";
+    x.style.display="flex";
+    x.style.padding="4px";
+    x.style.marginLeft="20%";
+    x.style.width="60%";
+    x.style.borderRadius="20px";
+    x.style.color="white";
+        
+}
+function myfunction4(){
+    var x=document.getElementById("a4");
+    document.getElementById("a2").style.display="none";
+    document.getElementById("a3").style.display="none";
+    document.getElementById("a1").style.display="none";
+    document.getElementById("a5").style.display="none";
+        x.style.display="flex";
+        x.style.padding="4px";
+        x.style.flexWrap="wrap";
+        x.style.justifyContent="space-evenly";
+        x.style.marginLeft="10%";
+        x.style.width="80%";
+        x.style.color="white";
+    
+        
+}
+function myfunction5(){
+    var x=document.getElementById("a5");
+    document.getElementById("a1").style.display="none";
+    document.getElementById("a2").style.display="none";
+    document.getElementById("a4").style.display="none";
+    document.getElementById("a3").style.display="none";
+    x.style.display="flex";
+    x.style.flexDirection="column";
+    x.style.padding="4px";
+    x.style.marginLeft="20%";
+    x.style.width="60%";
+    x.style.border="2px solid whitesmoke";
+    x.style.borderRadius="20px";
+    x.style.borderRadius="20px";
+    x.style.color="white";
+        
+}
